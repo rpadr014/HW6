1cb822f58514a65f1fbca21a1f06d14e9c8f2543
[LauraAurora] <[laura.apenza@gmail.com]>
2022-07-22 21:41:33 -0400

    Oath

---
+++ HW5ControlLibrary/PathGradient.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace HW6ControlLibrary
+{
+    internal class PathGradient : Panel
+    {
+        public Color colorOne { get; set; }
+        public Color colorTwo { get; set; }
+        public float angle { get; set; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, this.colorOne, this.colorTwo, this.angle);
+            Graphics graphic = e.Graphics;
+            graphic.FillRectangle(brush, this.ClientRectangle);
+            base.OnPaint(e);
+
+        }
+    }
+}
